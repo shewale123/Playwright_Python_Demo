b0ab12cc4d6f6c13d8c47d691f10d73bb75701ea
shewale123 <suvarnapatole0907@gmail.com>
2026-02-28 12:29:51 +0530

    Add initial Playwright demo script

---
+++ first_demotest.py
@@ -0,0 +1,13 @@
+
+print("Script started")
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)  # headless=True for instant output
+    page = browser.new_page()
+    page.goto("https://www.google.com")
+    print("Page title is:", page.title())
+    browser.close()
+
+print("Script finished")
